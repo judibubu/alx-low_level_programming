ba24932ea6e5c874ceef7f955d08fc63e7d04b31
judibubu <enwerejudechukwuebuka@gmail.com>
2022-09-12 15:27:44 -0700

    my commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints out the last part of a quote in the standard error.
+ *
+ * Return: 1 if success.
+ */
+int main(void)
+{
+	fwrite(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
+
