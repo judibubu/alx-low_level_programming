92a56e280380862f47fe30fd1e48df9028dd792c
judibubu <enwerejudechukwuebuka@gmail.com>
2022-09-19 16:51:51 -0700

    my commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * rev_string - a function that reverse a string
+ * followed by a new line.
+ * @s: an input string
+ * Return: Nothing
+ */
+void rev_string(char *s)
+{
+char *start_c, *end_c, c;
+int i, count;
+int length = 0;
+for (i = 0; s[i]; i++)
+{
+length++;
+}
+count = length;
+start_c = s;
+end_c = s;
+for (i = 0; i < count - 1; i++)
+{
+end_c++;
+}
+for (i = 0; i < count / 2; i++)
+{
+c = *end_c;
+*end_c = *start_c;
+*start_c = c;
+start_c++;
+end_c--;	
+}
+}
