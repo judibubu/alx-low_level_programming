feb2e0bdf87dffce6df5ddeac37653e59636b702
judibubu <enwerejudechukwuebuka@gmail.com>
2022-09-19 17:01:19 -0700

    my commit

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,8 +11,13 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-char *aux = dest;
-while (*src)
-*dest++ = *src++;
-return (aux);
+int i;
+i = 0;
+while (src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return (dest);
 }
