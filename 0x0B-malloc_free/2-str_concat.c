1c8316471179b7c088ac6574bb909e9172e3ec89
judibubu <enwerejudechukwuebuka@gmail.com>
2022-10-05 20:16:20 -0700

    my commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,57 +12,30 @@ char *str_concat(char *s1, char *s2)
 {
 unsigned int s1_length = 0;
 unsigned int s2_length = 0;
-
 	char *alloc_mem;
-
 	unsigned int i, j;
 
-
-
 	if (s1 == NULL)
-
 		s1 = "";
 
-
-
 	if (s2 == NULL)
-
 		s2 = "";
 
-
-
 	while (*(s1 + s1_length) != '\0')
-
 		s1_length++;
 
-
-
 	while (*(s2 + s2_length) != '\0')
-
 		s2_length++;
 
-
-
 	alloc_mem = malloc(1 + (s1_length *sizeof(*s1)) + (s2_length *sizeof(*s2)));
-
 	if (alloc_mem == NULL)
-
 		return ('\0');
 
-
-
 	for (i = 0; i < s1_length; i++)
-
 		alloc_mem[i] = *(s1 + i);
 
-
-
 	for (j = 0; j < s2_length; j++, i++)
-
 		alloc_mem[i] = *(s2 + j);
 
-
-
 	return (alloc_mem);
-
 }
